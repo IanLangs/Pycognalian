12fd91ec8db84ec93138f52763c7c2c1893eac81
Ian <supergachaworld4@gmail.com>
2025-12-07 17:03:49 -0300

    new hot fix for cpm

--- cpm.py
+++ cpm.py
@@ -1,5 +1,6 @@
 import sys, subprocess, re
 def main():
+    command = ["python", "-m", "pip"] + sys.argv
     pipr = subprocess.run(command, capture_output=True, text=True, stdout=subprocess.DEVNULL, check=True)
-    cpmr = re.sub(r"pip\d{0:5}", "cpm", pipr)
+    code = re.sub(r"pip(\d*)", lambda m:f"cpm{m.group(1)}", pipr.stdout +"\n"+ pipr.stderr)
     print(f"\n{code}\n")
